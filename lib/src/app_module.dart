c41800dd9c5569a1a698434daba0bc75de27f1d5
ElcioLS <elciols@gmail.com>
2023-05-13 14:03:57 -0300

    Aula4 - Criando Módulo

--- lib/src/app_module.dart
+++ lib/src/app_module.dart
@@ -1,5 +1,6 @@
 import 'package:academiadoflutter/src/modules/core/core_module.dart';
 import 'package:academiadoflutter/src/modules/payment_type/payment_type_module.dart';
+import 'package:academiadoflutter/src/modules/products/products_module.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 import 'modules/login/login_module.dart';
@@ -18,12 +19,14 @@ class AppModule extends Module {
             child: (context, args) => const BaseLayout(
                   body: RouterOutlet(),
                 ),
+            transition: TransitionType.noTransition,
             children: [
               // ChildRoute('/home', child: (context, args) => const HomePage()),
               ModuleRoute(
                 '/payment-type',
                 module: PaymentTypeModule(),
               ),
+              ModuleRoute('/products', module: ProductsModule())
             ]),
       ];
 }
---
+++ lib/src/modules/products/home/widgets/product_item.dart
@@ -0,0 +1,63 @@
+import 'package:academiadoflutter/src/core/extensions/formatter_extensions.dart';
+import 'package:academiadoflutter/src/core/ui/styles/text_styles.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../core/env/env.dart';
+
+class ProductItem extends StatelessWidget {
+  const ProductItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20),
+      ),
+      elevation: 10,
+      child: LayoutBuilder(builder: (_, constraints) {
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              height: constraints.maxHeight * .6,
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
+                ),
+                image: DecorationImage(
+                  image: NetworkImage(
+                    '${Env.instance.get('backend_base_url')}/storage/flutterweb.png',
+                  ),
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Text(
+                'Curso de Flutter Web ADF',
+                style: context.textStyles.textMedium,
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Text(29.94.CurrencyPTBR),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    'Editar',
+                  ),
+                )
+              ],
+            ),
+          ],
+        );
+      }),
+    );
+  }
+}
