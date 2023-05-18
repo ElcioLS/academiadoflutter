282b24cc4e40a8cd3af4e31777b79b95b56a5cbb
ElcioLS <elciols@gmail.com>
2023-05-18 01:22:20 -0300

    OrderRepository concluído

---
+++ lib/src/repositories/order/order_repository.dart
@@ -0,0 +1,11 @@
+import 'package:academiadoflutter/src/models/orders/order_status.dart';
+
+import '../../models/orders/order_model.dart';
+
+abstract class OrderRepository {
+  Future<List<OrderModel>> findAllOrders(DateTime date, [OrderStatus? status]);
+
+  Future<OrderModel> getById(int id);
+
+  Future<void> changeStatus(int id, OrderStatus status);
+}
